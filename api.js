c57f3f9591400eb1b59ca9d9316b75402405527e
Devanginisingh2002 <84927553+Devanginisingh2002@users.noreply.github.com>
2022-02-24 09:47:12 +0530

    Add files via upload
    Topic_4 Service APIs

---
+++ api.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Device = require('./models/device'); 
+mongoose.connect('mongodb+srv://devangini_ragh:devangini@cluster0.1wxho.mongodb.net/mydb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+const port = 5001;
+app.get('/api/devices', (req, res) => {
+  Device.find({}, (err, devices) => {
+    if (err == true) {
+      return res.send(err);
+    } else {
+      return res.send(devices);
+    }
+  });
+});
+
+
+app.post('/api/devices', (req, res) => {
+  const { name, user, sensorData } = req.body;
+  const newDevice = new Device({
+    name,
+    user,
+    sensorData
+  });
+  newDevice.save(err => {
+    return err
+      ? res.send(err)
+      : res.send('successfully added device and data');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
